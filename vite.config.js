cfc0f54b3b86a1e610df6ab4b74685c70d7ceb9a
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2025-11-12 09:31:26 +0800

    稍微修改一下

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
